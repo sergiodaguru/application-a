331f66e14db1f731ae2e9860458f3e98fb4a7a7c
sergiod <sergiod@us.ibm.com>
2023-06-17 22:09:36 -0400

    Interval

--- routes/messages.js
+++ routes/messages.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var router = express.Router();
-let text;
-let counter = 0;
+
+let ran = 0;
+function f() {
+   ran++;
+   console.log(ran)
+   setTimeout(f, 0);
+}
 
 /* GET greeting */
 router.get('/messages', function(req, res, next) {
-    let i
-     for (i = 0; i < 50; i++) {
-        text += "The number is " + i + "<br>";
-        console.log(text);
-     }
-     res.send({ "Sent Messages": i });
+  f();
+
+  setTimeout(() => { res.send(
+                         { "Messages": ran }
+                       ) }, 5000);
 });
 
 module.exports = router;
